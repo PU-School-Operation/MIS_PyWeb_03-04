065c7d7c0fc64fc5e9e72781344fd971fcfa9b24
DavidWu94 <davidwu010036@gmail.com>
2026-03-24 11:07:18 +0800

    deploy

--- test1.py
+++
@@ -1,5 +0,0 @@
-def function(x):
-    ans = 0
-    for i in range(x + 1):
-        ans += i
-    return ans
--- use03.py
+++
@@ -1,5 +0,0 @@
-from test1 import function
-
-x = int(input("請輸入一個正整數："))
-func = function(x)
-print(func)
